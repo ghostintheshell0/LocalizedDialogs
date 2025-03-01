ecfba6a1fc539b270363e4f56aabdc611bcf7dbc
sigeon <ghostintheshell404@gmail.com>
2025-03-01 17:41:22 +0300

    Add avatar for canvas sample / Move scripts to folder/ Remove useles folders from samples

--- Samples~/Shared/DialogCondition.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace LocalizedDialogs.Samples
-{
-    public abstract class DialogCondition : ScriptableObject
-    {
-        public abstract bool Check(Player player, NPC npc);
-    }
-}
